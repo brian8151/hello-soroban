9f541c22567212b349fe059021ad31476b3ba12a
Ubuntu <ubuntu@ip-172-31-19-204.ec2.internal>
2023-09-02 18:35:35 +0000

    Unit Test

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 #![no_std]
-
 use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
 
 #[contract]
-pub struct Contract;
+pub struct HelloContract;
 
 #[contractimpl]
-impl Contract {
-    /// Say Hello to someone or something.
-    /// Returns a length-2 vector/array containing 'Hello' and then the value passed as `to`.
+impl HelloContract {
     pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
         vec![&env, symbol_short!("Hello"), to]
     }
 }
+
+mod test;
--- src/test.rs
+++ src/test.rs
@@ -1,11 +1,13 @@
-use crate::{Contract, ContractClient};
+#![cfg(test)]
+
+use super::*;
 use soroban_sdk::{symbol_short, vec, Env};
 
 #[test]
-fn hello() {
+fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, Contract);
-    let client = ContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, HelloContract);
+    let client = HelloContractClient::new(&env, &contract_id);
 
     let words = client.hello(&symbol_short!("Dev"));
     assert_eq!(
